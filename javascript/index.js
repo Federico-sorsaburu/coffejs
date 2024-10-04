ef50011ade66cb8bfebe01c7a2f1c84c19cb397b
Federico-sorsaburu <Sorsaburu.federico98@gmail.com>
2024-10-04 09:11:54 -0300

    js

---
+++ javascript/index.js
@@ -0,0 +1,73 @@
+// Asignamos
+ 
+//  == operador de igualdad
+//  === operador de igualdad pero esctricto
+//  operador de igualdad pero estricto
+// != distinto
+// !== distinto estricto
+// caffe = 15
+// if(caffe == "20" ){
+//     console.log("Te alcanza un cafe nestle")
+//     // Iniciamos
+
+// }
+// let pedido = parseInt(prompt("Deseas: Cafe, Facturas o batido?"))  ;
+ 
+//  let pedidos = parseInt(prompt("Ingresa el presupuesto a abonar de lo que desees"));
+
+//  if(pedidos >= 70 ){
+//     console.log("cafe bonafide")
+//  }else if(pedidos >= 65){
+//     console.log("cafe torrado dulce")
+//  } else if (pedidos >= 50){
+//     console.log("cafe cortado")
+//  } else if (pedidos >= 45){
+//     console.log("cafe con leche")
+//  }else{
+//     console.log("no contas con saldo disponible para comprar :(")
+//  }
+
+
+//  a      b      a %% b
+// true    true    true(se va a ejecutar)
+
+
+
+/**
+ * ciclo por conteo
+ * operador imporante => incremento / decremento
+ * incrementar => ++ => i +1
+ * decrementar => -- => i -1
+ */
+
+
+// let preciosRelativos = 5;
+
+// for(let i = 1; i <=10; i++){
+
+
+//     let  precioResultado = preciosRelativos * i;
+//     console.log ( preciosRelativos + " x "+ i + " = " + precioResultado)
+// }
+
+
+
+ let password 
+
+ let intentos = 0;
+ 
+ const maxIntentosPermitidos = 2;
+
+ do{
+    password  = prompt ('ingresa la contraseña')
+    intentos++
+
+    if(password === )
+
+
+
+
+
+
+ }while(password !== 'clave123');
+ alert("Contraseña correcta")
